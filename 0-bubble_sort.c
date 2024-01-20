446027509b18e308b8fc3b4cfdcaa9ff5c201885
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-20 10:08:40 +0100

    added a check when list is null or less than 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,6 +13,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int tmp;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
