b7b3781a56559ff74328fe4a4a11f2ea825449d0
Dennis Adigwe <adigwedennis@gmail.com>
2024-01-20 09:51:47 +0100

    started sorting using bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ *		Bubble sort algorithm
+ * @array: the array of int to sort
+ * @size: the size of the @array
+ *
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
