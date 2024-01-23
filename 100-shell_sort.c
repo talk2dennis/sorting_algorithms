3a73778f961311f7fd988404c9325acadba97226
kinecon12 <kinecon12@gmail.com>
2024-01-23 15:27:35 -0800

    replace interger function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,1 +1,15 @@
 #include "sort.h"
+
+/**
+ * replace_ints - change two int in an array.
+ * @m: The first integer to swap.
+ * @n: The second integer to swap.
+ */
+void swap_ints(int *m, int *n)
+{
+	int tmps;
+
+	tmps = *m;
+	*m = *n;
+	*n = tmps;
+}
