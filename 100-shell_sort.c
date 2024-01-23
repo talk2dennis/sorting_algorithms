8ada5f635f675c2cab76dd72d782d9e4fdf20478
kinecon12 <kinecon12@gmail.com>
2024-01-23 15:33:52 -0800

    shell_sort function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -21,3 +21,28 @@ void swap_ints(int *m, int *n)
  * @size: The size of the array.
  * Description: Using Knuth interval sequence.
  */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
